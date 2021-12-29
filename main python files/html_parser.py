c7e0820676c344301579c71800e5d108a3607490
Gerald Freislich <freislichg@gmail.com>
2021-12-29 19:33:22 +1030

    commit

--- main python files/html_parser.py
+++ main python files/html_parser.py
@@ -16,7 +16,7 @@ google_url = "https://www.google.com"
 """Functions"""
 def checkingelements(textline):  # function to check whether or not an element is a html element and to add the element to a dictionary if it is
     # if statements to determine whether or not to add the data in the dictionary
-    html_tag_list = ("<a>", "<address>", "<area>", "<article>", "<aside>", "<audio>", "<b>",
+    html_open_tag_list = ["<a>", "<address>", "<area>", "<article>", "<aside>", "<audio>", "<b>",
     "<base>", "<bdo>", "<blockquote>", "<body>", "<br />", "<button>", "<canvas>",
     "<caption>", "<cite>", "<code>", "<col>", "<colgroup>", "<command>", "<datagrid>", 
     "<datalist>", "<dd>", "<del>", "<details>", "<dfn>", "<dir>", "<div>", "<dl>",
@@ -26,12 +26,22 @@ def checkingelements(textline):  # function to check whether or not an element i
     "<img>", "<input>", "<ins>", "<kbd>", "<keygen>", "<label>", "<legend>",
     "<li>", "<link>", "<map>", "<mark>", "<menu>", "<meta>", "<meter>", "<nav>", "<noscript>", "<object>",
     "<ol>", "<optgroup>", "<option>", "<output>", "<p>", "<param>", "<pre>", "<progress>", "<q>",
-    "<rp>", "<rt>", "<ruby>", "<s>", "<samp>", "<script>", "<section>", "<select>", "<source>", "<span>", "<strong>", "<style>", "<sub>", "<sup>", "<table>", "<tbody>", "<td>", "<textarea>", "<tfoot>", "<th>", "<thead>", "<time>", "<title>", "<tr>", "<track>", "<u>", "<ul>", "<var>", "<video>", "<wbr>" )
-
-    for tag in html_tag_list:
+    "<rp>", "<rt>", "<ruby>", "<s>", "<samp>", "<script>", "<section>", "<select>", "<source>", "<span>", "<strong>", "<style>", "<sub>", "<sup>", "<table>", "<tbody>", "<td>", "<textarea>", "<tfoot>", "<th>", "<thead>", "<time>", "<title>", "<tr>", "<track>", "<u>", "<ul>", "<var>", "<video>", "<wbr>", "<a", "<address", "<area", "<article", "<aside", "<audio", "<b",
+    "<base", "<bdo", "<blockquote", "<body", "<br /", "<button", "<canvas",
+    "<caption", "<cite", "<code", "<col", "<colgroup", "<command", "<datagrid", 
+    "<datalist", "<dd", "<del", "<details", "<dfn", "<dir", "<div", "<dl",
+    "<dt", "<em", "<embed", "<fieldset", "<figcaption", "<figure", "<font",
+    "<footer", "<form", "<frame", "<frameset", "<h1", "<h2", "<h3", "<h4", "<h5",
+    "<h6", "<head", "<header", "<hgroup", "<hr /", "<html", "<i", "<iframe",
+    "<img", "<input", "<ins", "<kbd", "<keygen", "<label", "<legend",
+    "<li", "<link", "<map", "<mark", "<menu", "<meta", "<meter", "<nav", "<noscript", "<object",
+    "<ol", "<optgroup", "<option", "<output", "<p", "<param", "<pre", "<progress", "<q",
+    "<rp", "<rt", "<ruby", "<s", "<samp", "<script", "<section", "<select", "<source", "<span", "<strong", "<style", "<sub", "<sup", "<table", "<tbody", "<td", "<textarea", "<tfoot", "<th", "<thead", "<time", "<title", "<tr", "<track", "<u", "<ul", "<va", "<video", "<wbr"]    
+    
+    html_closed_tag_list = []
+    for tag in html_open_tag_list:
         if tag in textline:
             print(textline)
-
 
 """Parser to find the main html elements"""
 class HtmlParser():
@@ -71,6 +81,6 @@ class HtmlParser():
         for row in doc:
             checkingelements(textline=row)
 
-htmlparser = HtmlParser(base_url)
+htmlparser = HtmlParser(google_url)
 htmlparser.iterthroughfiles()
 
