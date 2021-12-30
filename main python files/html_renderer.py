0fc735c809c07f9330025fd5e91217bd09e5f10c
Gerald Freislich <freislichg@gmail.com>
2021-12-30 19:00:51 +1030

    c

--- main python files/html_renderer.py
+++ main python files/html_renderer.py
@@ -5,6 +5,14 @@ from tkinter import *
 """Modular imports"""
 from html_parser import HtmlParser, returninghtmllist
 
+
+"""Functions"""
+def atagwithargs(window, contents, rownumber):
+    Label(master=window, text=contents).grid(row=rownumber, column=40)
+
+def atagwithoutargs(window, contents, rownumber):
+    print('flip')
+
 class HtmlRenderer():
     def __init__(self, mainwindow, searchedurl):
         self.rendering_window = mainwindow
