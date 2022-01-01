e8f1a01ec57ade276fb49c1528f41c69d0ec2cf8
Gerald Freislich <freislichg@gmail.com>
2022-01-01 12:51:09 +1030

    commit

--- main python files/html_linked_commands.py
+++ main python files/html_linked_commands.py
@@ -1,3 +1,5 @@
+import tkinter as tk
+from tkinter import *
 """Functions"""
 
 # html tags with arguments
--- main python files/html_renderer.py
+++ main python files/html_renderer.py
@@ -14,7 +14,26 @@ class HtmlRenderer():
 
         self.html_list = returninghtmllist()
 
-    def linking_commands(self):
-        for items in self.html_list:
-            print(items)
+        self.row_number = 0
 
+    def linking_commands(self):
+
+        for items in self.html_list:
+
+            self.row_number += 1
+
+            if items[0] == "<a ":
+                atagwithargs(self.rendering_window, items[2], self.row_number)
+            
+            elif items[0] == "<address ":
+                addresswithargs(self.rendering_window, items[2], self.row_number)
+        
+        self.rendering_window.mainloop()
+
+
+main_window = tk.Tk()
+main_window.geometry("900x600+210+0")
+main_window.config(bg="white")
+
+html_render = HtmlRenderer(main_window, "https://www.google.com")
+html_render.linking_commands()
